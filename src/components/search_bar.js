c0d7e174df1cb876f95c25aa8e29a7dbe30b1a3e
Paritosh Kumar <kumar.paritosh20@gmail.com>
2018-04-15 20:57:52 +0530

    Added search functionality and throttling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render(){
-    return <input />;
+  // this is how we initialize state in a class based component
+  constructor(props) {
+    super(props);
+    // we are calling the parent method (i.e method of Component) with super
+
+    this.state = { term:'Search For Videos' };
+    //this 'term' refers to the input of the user, or the search term
+    //thus whenever some event will occur we will change the value of the state or the 'term'
   }
+
+  render(){
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+           //by doing this we are making the input component a controlled component
+          onChange={(event) => this.onInputChange(event.target.value)} />
+      </div>
+    )
+  }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import _ from 'lodash';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_data';
 
 const API_KEY = 'AIzaSyDNmNxcSIGF1j9hZBh5hWmPDwrN2ek4LVg';
 
+// Creating a new component
 
-//creating a new component
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key:API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+
+      // this is equal to -> this.setState({ videos: videos })
+      // this will only work when the key is same as the property name
+
+    });
+  }
+
+  render(){
+  const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 500);
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={videoSearch} />
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos} />
     </div>
   );
+  }
 }
 
-//take this compenent and generate html
+// Take this compenent and generate html
 ReactDOM.render(<App />,document.querySelector('.container'));
